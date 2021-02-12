7db473f5f40e7fbc546ec69fed708a46dc342c45
thanigai0329 <rajagopal0329@gmail.com>
2021-02-12 09:31:52 +0530

    Create RoundOne.js

---
+++ RoundOne.js
@@ -0,0 +1,38 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const rlWithPromise = (question) => {
+  return new Promise((resolve, reject) => {
+    rl.question("", (data) => {
+      resolve(data);
+    });
+  });
+};
+
+const main = async () => {
+  let inputText = await rlWithPromise("");
+  let inputTextLength = inputText.length;
+  let midText = parseInt(inputTextLength / 2);
+  for (let i = 0; i < inputTextLength; i++) {
+    let text = "";
+    for (let j = i; j < inputTextLength - 1; j++) {
+      text = text + "  ";
+    }
+    for (let l = -1, k = midText; l < i; l++) {
+      if (k < inputTextLength || k < midText) {
+        text += inputText[k] + " ";
+        k++;
+      } if (k == inputTextLength - 1) {
+        text += inputText[k] + " ";
+        k = 0;
+      }
+    }
+    console.log(text);
+  }
+  rl.close();
+};
+
+main();
